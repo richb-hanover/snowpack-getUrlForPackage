2c61ad43150f63e08a17a397944f6c0853fdb4d8
Rich Brown <richb.hanover@gmail.com>
2021-03-27 08:03:11 -0400

    Commit of working project

---
+++ snowpack.config.js
@@ -0,0 +1,55 @@
+// // Snowpack Configuration File
+// // See all supported options: https://www.snowpack.dev/reference/configuration
+
+// /** @type {import("snowpack").SnowpackUserConfig } */
+// module.exports = {
+//   mount: {
+//     /* ... */
+//   },
+//   plugins: [
+//     /* ... */
+//   ],
+//   packageOptions: {
+//     /* ... */
+//   },
+//   devOptions: {
+//     /* ... */
+//   },
+//   buildOptions: {
+//     /* ... */
+//   },
+// };
+
+/** @type {import("snowpack").SnowpackUserConfig } */
+module.exports = {
+  mount: {
+    // public: { url: "/", static: true },
+    // src: { url: "/dist" },
+  },
+  plugins: [
+    // ["@snowpack/plugin-typescript"],
+    // ["@snowpack/plugin-webpack"],
+    // ["./inject-version.js", {}],
+  ],
+  // install: [
+  //   /* ... */
+  // ],
+  installpackageOptions: {
+    // installTypes: true,
+  },
+  packageOptions: {
+    // source: "remote",
+  },
+  devOptions: {
+    /* ... */
+  },
+  buildOptions: {
+    /* ... */
+  },
+  // proxy: {
+  //   /* ... */
+  // },
+  alias: {
+    /* ... */
+  },
+};
